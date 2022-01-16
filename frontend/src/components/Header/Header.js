29767f55eab3013a70c9212fa3fc6e34bd5bc1a3
Akash <akash.patel@nblocks.org>
2022-01-16 22:54:19 +0530

    feat : added header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Header.css';
 
-export const Header = () => {
+export const Header = ({userName}) => {
   return (
-    <div>
-      <h2>Header</h2>
-    </div>
+    <header className="header">
+      <nav>
+        <div className="leftPortion">
+          <div className="title">Note App</div>
+          <div className="searchBar">
+            <input className="search" type={'search'}></input>
+          </div>
+        </div>
+        <div className="rightPortion">
+          <div className="myNotes">My Notes</div>
+          <div className="userInfo">{userName}</div>
+        </div>
+      </nav>
+    </header>
   );
 };
