33f49edbb0e9ecf751f64e98d4acf71296a86da6
Akash <akashpatel18041999@gmail.com>
2022-02-27 19:11:44 +0530

    feat : added login sign up functionality

--- frontend/src/components/Buttons/LoginButton.js
+++ frontend/src/components/Buttons/LoginButton.js
@@ -1,6 +1,12 @@
 import {Button} from '@mui/material';
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export default function LoginButton() {
-  return <Button>Login</Button>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/login');
+  };
+
+  return <Button onClick={handleClick}>Login</Button>;
 }
--- frontend/src/components/Buttons/SignUpButton.js
+++ frontend/src/components/Buttons/SignUpButton.js
@@ -1,6 +1,11 @@
 import {Button} from '@mui/material';
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export const SignUpButton = () => {
-  return <Button>Sign Up</Button>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/register');
+  };
+  return <Button onClick={handleClick}>Sign Up</Button>;
 };
