35db92cb1707ee251d8fd2a45fe5f26346363cba
Akash <akashpatel18041999@gmail.com>
2022-02-28 11:17:39 +0530

    feat : added single note comp

--- frontend/src/screens/SingleNote/SingleNote.js
+++ frontend/src/screens/SingleNote/SingleNote.js
@@ -1,6 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams, useNavigate} from 'react-router-dom';
+import {
+  Container,
+  InputLabel,
+  FormControl,
+  Input,
+  FormHelperText,
+  TextField,
+  Typography,
+  Button,
+} from '@mui/material';
+import ReactMarkdowm from 'react-markdown';
 
 export const SingleNote = () => {
   const {id} = useParams();
@@ -19,10 +30,47 @@ export const SingleNote = () => {
   const [date, setDate] = useState();
 
   return (
-    <div>
-      <p>{note?.title}</p>
-      <p>{note?.content}</p>
-      <p>{note?.category}</p>
-    </div>
+    <Container
+      fixed
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: '90%',
+        width: 700,
+      }}>
+      <FormControl>
+        <TextField
+          varient={'outlined'}
+          // label={'Title'}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          style={{marginTop: 20, marginBottom: 20}}
+        />
+        <TextField
+          varient={'outlined'}
+          // label={'Content'}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          style={{marginTop: 20, marginBottom: 20}}
+          multiline
+          rows={2}
+          maxRows={4}
+        />
+        {content && (
+          <>
+            <Typography>Note Preview :</Typography>
+            <ReactMarkdowm>{content}</ReactMarkdowm>
+          </>
+        )}
+        <TextField
+          value={category}
+          varient={'outlined'}
+          // label={'Category'}
+          onChange={(e) => setCategory(e.target.value)}
+          style={{marginTop: 20, marginBottom: 20}}
+        />
+        <Button onClick={() => dispatch()}>Update Note</Button>
+      </FormControl>
+    </Container>
   );
 };
